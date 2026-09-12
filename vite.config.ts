95d2c1f786f2204ad45005702ff2a8f776b8aac1
Chedrick Daniel Uzaga López <80350684+Chedrick343@users.noreply.github.com>
2026-09-11 18:37:52 -0600

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Laboratorio-2-Comercio-electronico',
+  base: '/Proyecto1Comercio',
   plugins: [react()],
 })
